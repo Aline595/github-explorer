a32c03be3aa898870f1d9d2f03ee196d3214d29a
unknown <aline.as385@gmail.com>
2020-11-06 14:03:21 -0300

    Adicionando rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+// import Users from '../pages/Users';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  // switch para mostar apenas uma rota
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
