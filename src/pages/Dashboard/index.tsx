98451499eb9554c35e75c9fe2eee5cc8defc95ea
unknown <aline.as385@gmail.com>
2020-11-06 15:19:54 -0300

    Página dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import { FiChevronRight, FiX } from 'react-icons/fi';
 
-import { Title, Form } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 // fc -> componente escrito em formato de função
 const Dashboard: React.FC = () => {
   return (
     <>
+      <img src={logo} alt="Github Explorer" />
       <Title>Explore os usuários e seus repositórios no Github</Title>
+
       <Form>
         <input type="text" placeholder="Digite o nome do usuário do Github" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/56769013?s=460&u=a4f3390f46e8180496d3a99749f3e70b7402ae9e&v=4"
+            alt="Aline Soares da Silva"
+          />
+          <div>
+            <strong>Aline595/teste</strong>
+            <p>Descrição do usuário do github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -8,33 +8,39 @@ export const Title = styled.h1`
   max-width: 450px;
   font-size: 48px;
   line-height: 56px;
+
   margin-top: 80px;
 `;
 
 export const Form = styled.form`
   margin-top: 40px;
   max-width: 715px;
+
   display: flex;
+
   input {
     flex: 1;
     height: 70px;
     padding: 0 24px;
     border: 0;
-    border-radius: 5px;
+    border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+
     &::placeholder {
       color: #a8a8b3;
     }
   }
+
   button {
-    width: 240px;
+    width: 210px;
     height: 70px;
     background: #04d361;
-    border-radius: 5px;
+    border-radius: 0px 5px 5px 0px;
     border: 0;
     color: #fff;
     font-weight: bold;
     transition: background 0.2s;
+
     &:hover {
       background: ${shade(0.2, '#04d361')};
     }
@@ -52,31 +58,44 @@ export const Form = styled.form`
   }
 `;
 
-export const GithubUserList = styled.div`
+export const Repositories = styled.div`
   margin-top: 80px;
   max-width: 700px;
-  display: flex;
-  background: #fff;
-  padding: 24px;
-  align-items: center;
+
   a {
+    background: #fff;
     border-radius: 5px;
     width: 100%;
+    padding: 24px;
     display: block;
     text-decoration: none;
+
     display: flex;
     align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
     img {
       width: 64px;
       height: 64px;
       border-radius: 50%;
     }
+
     div {
-      margin: 0 16px;
+      margin: 16px;
+
       strong {
-        font-size: 20px;
+        font-size: 18px;
         color: #3d3d4d;
       }
+
       p {
         font-size: 18px;
         color: #a8a8b3;
@@ -84,9 +103,11 @@ export const GithubUserList = styled.div`
       }
     }
   }
-  > svg {
-    color: #c9c9d4;
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
   }
+
   button {
     margin-left: 10px;
     background: none;
