86bd0655d46de68e0c5d35bde8500b0786f67af7
unknown <aline.as385@gmail.com>
2020-11-10 14:41:52 -0300

    Navegação entre as telas

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 // fc -> componente escrito em formato de função
 const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
   return <h1>Repository</h1>;
 };
 
