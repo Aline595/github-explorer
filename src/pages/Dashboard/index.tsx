31f915ffaa01d8092976838dd9547958612a0c04
unknown <aline.as385@gmail.com>
2020-11-10 14:16:41 -0300

    Tratando erros

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 // Colocar somente informações que será usado e não necessariamente tudo que será retornado  pela API
 interface Repository {
@@ -16,32 +16,45 @@ interface Repository {
   };
 }
 
+// truthy -> Tudo q não é de valor vazio
+// falsy -> Tudo que é de valor vazio
+
 // fc -> componente escrito em formato de função
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handlerAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
+    // Adição de um novo repositório
     // Não deixa recarregar a página
     event.preventDefault();
-    // Adição de um novo repositório
+    // Verifica se foi digitado um repositório
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório.');
+      return;
+    }
+    try {
+      // Consumir API do Github
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
 
-    // Consumir API do Github
-    const response = await api.get<Repository>(`repos/${newRepo}`);
-    const repository = response.data;
-
-    // Salvar novo repositório no estado
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+      // Salvar novo repositório no estado
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório.');
+    }
   }
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explore os usuários e seus repositórios no Github</Title>
 
-      <Form onSubmit={handlerAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handlerAddRepository}>
         <input
           value={newRepo}
           onChange={e => setNewRepo(e.target.value)}
@@ -50,6 +63,8 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
         {repositories.map(repository => (
